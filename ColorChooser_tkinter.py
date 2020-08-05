3a9526e01f582323a48d6d7712d46235849036d9
Papa <aroapu@mail.ru>
2020-08-05 13:39:54 +0300

    Modified txt cap example

--- ColorChooser_tkinter.py
+++ ColorChooser_tkinter.py
@@ -2,4 +2,13 @@
 import tkinter.colorchooser
 
 col = tkinter.colorchooser.askcolor()
-print(col)
+print(col[1])
+
+# from tkinter import *
+# from tkinter.colorchooser import *
+# def getColor():
+#     color = askcolor()
+#     print(color[1])
+#     # print(type(color))
+# Button(text='Select Color', command=getColor).pack()
+# mainloop()
